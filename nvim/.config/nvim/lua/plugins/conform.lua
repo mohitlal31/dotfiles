7965aa5f3b3e413c85ef089a27432ee6952038d3
Mohit Lal <mohitlal31@gmail.com>
2025-12-25 08:04:01 +0530

    Add prettier formatter for markdown in LazyVim

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      ["markdown"] = { "prettier" },
+    },
+    formatters = {
+      prettier = {
+        prepend_args = { "--prose-wrap", "always", "--print-width", "80" },
+      },
+    },
+  },
+}
